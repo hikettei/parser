e5aeb5cab00d0b8ae432f52f55323d282a460f3c
hikettei <hikettei@gmail.com>
2021-08-17 14:32:11 +0900

    名前の重複に対応

--- syntax.lisp
+++ syntax.lisp
@@ -3,14 +3,14 @@
 
 (define-rule args vars nums)
 
-(define-char vars #'(lambda (x) (typep x 'symbol)))
-(define-char nums #'(lambda (n) (typep n 'number)))
-
-(define-syntax *exp* var-name vars) ; 
-(define-syntax *exp* numbers   nums) ; 
+(define-char *exp* vars #'(lambda (x) (typep x 'symbol)))
+(define-char *exp* nums #'(lambda (n) (typep n 'number)))
+(define-char *exp* setq #'(lambda (n) (eq n 'setq)))
 
 (define-syntax *exp* nest     vars funcall funcall)
-
-(define-syntax *exp* funcall  vars numbers numbers)
-(define-syntax *exp* addnum   nums var-name numbers)
+(define-syntax *exp* addnum   nums vars nums)
+(define-syntax *exp* nest1 vars nest nest)
+(define-syntax *exp* set-query vars vars setq nums)
+;(define-syntax *exp* SyntaxName CHAR SYNTAX SYNTAX...) <- (仮)
+;(define-syntax *exp* callfunc1 vars nest)
 ; (tokenizer '(+ 1 + 1 1)) = (+ 1 (+ 1 1))
